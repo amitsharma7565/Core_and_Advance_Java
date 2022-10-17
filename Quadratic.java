5cd032e643ba5f13a70dfd1b7bb268f6282bf381
amitsharma7565 <112172530+amitsharma7565@users.noreply.github.com>
2022-10-17 18:07:59 +0530

    Add files via upload

---
+++ Quadratic.java
@@ -0,0 +1,11 @@
+class Quadratic{
+  public static void main(String args[]){
+    //to solve the quadratic equation
+    int a,b,c,result;
+    a=5;
+    b=6;
+    c=10;
+    result=(b*b)-4*a*c;
+    System.out.println(result);
+  }
+}

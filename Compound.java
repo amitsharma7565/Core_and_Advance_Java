105462240396eb7048a0b13e4d9f29b9bf8eb3c7
amitsharma7565 <112172530+amitsharma7565@users.noreply.github.com>
2022-10-18 13:58:50 +0530

    Add files via upload

---
+++ Compound.java
@@ -0,0 +1,13 @@
+class Compound{
+  public static void main(String args[]){
+    int p,r,t,si,ci;
+    p=10000;
+    r=200;
+    t=3;
+    si=(p*r*t)/100;
+    ci=si-p;
+   System.out.println(si);
+   System.out.println(ci);
+    
+  }
+}

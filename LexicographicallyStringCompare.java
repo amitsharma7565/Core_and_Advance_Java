97c63b96cc58fedfe5d6d200d2d1d84e126b9d60
Amit Sharma <112172530+amitsharma7565@users.noreply.github.com>
2023-08-25 12:09:15 +0530

    Add files via upload

---
+++ LexicographicallyStringCompare.java
@@ -0,0 +1,6 @@
+class LexicographicallyStringCompare{
+	String str1 = "This is Exercise 1";
+        String str2 = "This is Exercise 2";
+    	int num=str1.compareTo(str2);
+    	System.out.println(num);
+}
